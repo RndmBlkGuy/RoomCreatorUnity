c6938814c3895acd1335ee40ccc348ff4a8d41dd
RndmBlkGuy <dmwilliams928@gmail.com>
2020-10-16 16:15:26 -0500

    Refactored GenerateRoom class to create gameobjects as components. now can call methods like createTopLeftCorner to create the two walls and CreateSingleFloor to create the floor.
    Added ability to create hallways

--- GenerateRooms.cs
+++ GenerateRooms.cs
@@ -13,6 +13,8 @@ public class GenerateRooms : EditorWindow
 
     [SerializeField] private float wallEdgePlacement;
 
+    [SerializeField] private float hallWayLength;
+
     [MenuItem("Tools/Generate Rooms")]
     static void CreateGenerateRooms()
     {
@@ -20,19 +22,26 @@ public class GenerateRooms : EditorWindow
     }
     private void OnGUI()
     {
+        //All the prefabs used to create the room and the offset if there is one.
+        //Note: will Change offset to calculate WallOffset = y/2
         tile = (GameObject)EditorGUILayout.ObjectField("Prefab tile", tile, typeof(GameObject), false);
         wall = (GameObject)EditorGUILayout.ObjectField("Prefab wall", wall, typeof(GameObject), false);
         ceiling = (GameObject)EditorGUILayout.ObjectField("Prefab Ceiling", ceiling, typeof(GameObject), false);
-
+        wallEdgePlacement = EditorGUILayout.FloatField("Place Wall on edge pos", wallEdgePlacement );
         
 
-         //EditorGUILayout.LabelField("width: ", roomWidth );
+         EditorGUILayout.LabelField("Create Room" );
          
 
         roomWidth = EditorGUILayout.IntField("Room Width", roomWidth);
         roomLength = EditorGUILayout.IntField("Room length", roomLength);
 
-        wallEdgePlacement = EditorGUILayout.FloatField("Place Wall on edge pos", wallEdgePlacement );
+        
+
+        EditorGUILayout.LabelField("Create Hallway" );
+
+        hallWayLength = EditorGUILayout.IntField("Hallway length", roomLength);
+
 
         //roomLength = EditorGUILayout.ObjectField("Room Height", roomLength, typeof(int), false);
 
@@ -51,16 +60,14 @@ public class GenerateRooms : EditorWindow
                     for (var j = 0; j <= roomWidth; j++)
                     {
                         //Place floor piece in place
-                        Vector3 pos = new Vector3(i, 0, j);
-                        Instantiate(tile, pos, tile.transform.rotation);
-
-                        Vector3 pos1 = new Vector3(i, 1, j);
-                        Quaternion rot2 = Quaternion.Euler(180.0f, 0.0f, 0.0f);
-                        Instantiate(ceiling, pos1, ceiling.transform.rotation);
+                        CreateSingleFloor(i, 0, j);
+                        
+                        //CreateSingleCeiling(i, 0, j);
+                        
 
                         //If corner, create two walls to make corner
                         //if((i == 0)|| (j == 0)){
-                            CreateCorner(i, j, roomWidth, roomLength);
+                            CreateCorners(i, j, roomWidth, roomLength);
                         //}
 
                         //If roomLength is larger that 2 but on edge, create walls on the long side edges
@@ -81,49 +88,40 @@ public class GenerateRooms : EditorWindow
             }
          
         }
+
+        if (GUILayout.Button("Create hallway"))
+        {
+            for(int i = 0; i < hallWayLength; i++){
+
+                CreateSingleFloor(0, 0, i);
+                CreateWallWidthTop(0, 0, i, wallEdgePlacement);
+                CreateWallWidthBottom(0, 0, i, wallEdgePlacement);
+            }
+        }
         GUI.enabled = false;
         //EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
     }
 
 //create corner
-    void CreateCorner(int i, int j, float wallSideW, float wallSideL){
+    void CreateCorners(int i, int j, float wallSideW, float wallSideL){
         if (((i == 0) && (j == 0)) )
         {
-            Vector3 pos2 = new Vector3(0, i + wallEdgePlacement, j - wallEdgePlacement);
-            Instantiate(wall, pos2, wall.transform.rotation);
-
-            Vector3 pos3 = new Vector3(i - wallEdgePlacement, j + wallEdgePlacement, 0);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
+            CreateTopLeftCorner(i, 0, j, wallEdgePlacement);
         }
 
-        if ((i == 0) && (j == roomWidth))
+        else if ((i == 0) && (j == roomWidth))
         {
-            Vector3 pos2 = new Vector3(0, i + wallEdgePlacement, j + wallEdgePlacement);
-            Instantiate(wall, pos2, wall.transform.rotation);
+            CreateTopRightCorner(i, 0, j, wallEdgePlacement);
 
-            Vector3 pos3 = new Vector3(i - wallEdgePlacement, wallEdgePlacement, j);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
 
         }
 
-        if ((i == wallSideL) && (j == 0)){
-            Vector3 pos2 = new Vector3(i, wallEdgePlacement, j - wallEdgePlacement);
-            Instantiate(wall, pos2, wall.transform.rotation);
-
-            Vector3 pos3 = new Vector3(i+wallEdgePlacement, wallEdgePlacement, j);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
+        else if ((i == wallSideL) && (j == 0)){
+            CreateBottomLeftCorner(i, 0, j, wallEdgePlacement);
         }
 
-        if ((i == wallSideL) && (j == wallSideW)){
-            Vector3 pos2 = new Vector3(i, wallEdgePlacement, j + wallEdgePlacement);
-            Instantiate(wall, pos2, wall.transform.rotation);
-
-            Vector3 pos3 = new Vector3(i+wallEdgePlacement, wallEdgePlacement, j);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
+        else if ((i == wallSideL) && (j == wallSideW)){
+            CreateBottomRightCorner(i, 0, j, wallEdgePlacement);
         }
 
     }
@@ -135,22 +133,16 @@ public class GenerateRooms : EditorWindow
         if ((i == 0) && ((j > 0) && (j < wallSideW)))
         {
 
-            Vector3 pos3 = new Vector3(i - wallEdgePlacement, wallEdgePlacement, j);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
+            CreateWallWidthTop(i, 0, j, wallEdgePlacement);
 
         }
 //bottom
         else if ((i == wallSideL) && ((j > 0) && (j < wallSideW)))
         {
 
-            Vector3 pos3 = new Vector3(i + wallEdgePlacement, wallEdgePlacement, j);
-            Quaternion rot2 = Quaternion.Euler(0.0f, 90.0f, 0.0f);
-            Instantiate(wall, pos3, rot2);
+            CreateWallWidthBottom(i, 0, j, wallEdgePlacement);
 
         }
-
-
 
     }
 
@@ -158,22 +150,110 @@ public class GenerateRooms : EditorWindow
     void CreateWallsLength(int i, int j, float wallSideW, float wallSideL){
 
 
-        if (((i > 0) && (i < wallSideL)) && ((j == 0) || (j == wallSideW)))
+        if (((i > 0) && (i < wallSideL)) && ((j == 0)))
         {
 
-            if ((j == wallSideW))
-            {
-                Vector3 pos1 = new Vector3(i, wallEdgePlacement, j + wallEdgePlacement);
-                Instantiate(wall, pos1, wall.transform.rotation);
-            }
-            else
-            {
-                Vector3 pos2 = new Vector3(i, wallEdgePlacement, j - wallEdgePlacement);
-                Instantiate(wall, pos2, wall.transform.rotation);
-            }
-
-
+        CreateWallLengthLeft(i, 0, j, wallEdgePlacement);
+            
         }
 
+        else if ( ((i > 0) && (i < wallSideL)) && (j == wallSideW)){
+
+         CreateWallLengthRight(i, 0, j, wallEdgePlacement);
+        }
+
+    }
+
+    //Create Top Left Coner Wall
+    void CreateTopLeftCorner(float x, float y, float z, float wallOffset)
+    {
+
+        Vector3 pos = new Vector3(x, y + wallOffset, z - wallOffset);
+        Instantiate(wall, pos, wall.transform.rotation);
+
+        pos = new Vector3(x - wallOffset, y + wallOffset, 0);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create Top Right Coner Wall
+    void CreateTopRightCorner(float x, float y, float z, float wallOffset)
+    {
+
+        Vector3 pos = new Vector3(x, y + wallOffset, z + wallOffset);
+        Instantiate(wall, pos, wall.transform.rotation);
+
+        pos = new Vector3(x - wallOffset, wallOffset, z);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create Bottom Left Coner Wall
+    void CreateBottomLeftCorner(float x, float y, float z, float wallOffset)
+    {
+
+        Vector3 pos = new Vector3(x, wallOffset, z - wallOffset);
+        Instantiate(wall, pos, wall.transform.rotation);
+
+        pos = new Vector3(x + wallOffset, wallOffset, z);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create Bottom Right Coner Wall
+    void CreateBottomRightCorner(float x, float y, float z, float wallOffset)
+    {
+
+        Vector3 pos = new Vector3(x, wallOffset, z + wallOffset);
+        Instantiate(wall, pos, wall.transform.rotation);
+
+        pos = new Vector3(x + wallOffset, wallOffset, z);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create single tile floor
+    void CreateSingleFloor(float x, float y, float z)
+    {
+        Vector3 pos = new Vector3(x, y, z);
+        Instantiate(tile, pos, tile.transform.rotation);
+    }
+
+    //Create single tile ceiling
+    void CreateSingleCeiling(float x, float y, float z)
+    {
+        Vector3 pos1 = new Vector3(x, 1, z);
+        Quaternion rot2 = Quaternion.Euler(180.0f, 0.0f, 0.0f);
+        Instantiate(ceiling, pos1, ceiling.transform.rotation);
+    }
+
+    //Create wall to the top of the space
+    void CreateWallWidthTop(float x, float y, float z, float wallOffset)
+    {
+        Vector3 pos = new Vector3(x - wallOffset, wallOffset, z);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create wall to the bottom of the space
+    void CreateWallWidthBottom(float x, float y, float z, float wallOffset)
+    {
+        Vector3 pos = new Vector3(x + wallOffset, wallOffset, z);
+        Quaternion rot = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        Instantiate(wall, pos, rot);
+    }
+
+    //Create left wall
+    void CreateWallLengthLeft(float x, float y, float z, float wallOffset)
+    {
+        Vector3 pos2 = new Vector3(x, wallOffset, z - wallOffset);
+        Instantiate(wall, pos2, wall.transform.rotation);
+    }
+
+    //Create right wall
+    void CreateWallLengthRight(float x, float y, float z, float wallOffset)
+    {
+        Vector3 pos = new Vector3(x, wallOffset, z + wallOffset);
+        Instantiate(wall, pos, wall.transform.rotation);
     }
 }
